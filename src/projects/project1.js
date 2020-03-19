48ec659ab29c6c656cf684c2024a719319435807
Isabel Klein <52258290+isabelklein@users.noreply.github.com>
2020-03-18 20:48:11 -0700

    Add projects

--- src/projects/project1.js
+++ src/projects/project1.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import image1 from '../images/project1/Home.png'
 
 class Project1 extends Component {
     state = {}
@@ -6,10 +7,42 @@ class Project1 extends Component {
     render() {
         return (
             //Project Title
-            <div> 
-           <div className='project'><h1>Tuberculosis Treatment Companion</h1></div>
-           <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-           </div>
+            <div className='project'> 
+            <h1>Tuberculosis Treatment Companion</h1>
+            <h2>A mobile web app to support tuberculosis patients and care providers</h2>
+
+            <table>
+                <thead>
+                    <th>Background</th>
+                    <th>My Role</th>
+                    <th>Team</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Treatment Companion is a mobile web app to help support tuberculosis patients and care providers and increase treatement completion rates. It was deployed in a 6-month pilot study in Argentina in 2019. My team redesigned the app based on pilot study feedback.</td>
+                        <td>
+                            UX Design Team Member
+                            <ul>
+                                <li>UX/UI Design</li>
+                                <li>User Flows</li>
+                                <li>Wireframing</li>
+                                <li>Prototyping</li>
+                            </ul>
+                        </td>
+                        <td>Design team of UW students and faculty including
+                            <ul>
+                                <li>4 UX Designers</li>
+                                <li>1 User Researcher</li>
+                                <li>1 Data Analyst</li>
+                                <li>1 Faculty Director</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <img src={image1} alt='Treatment Companion App' style={{height: 30 + 'em'}}></img>
+            </div>
         );
     }
 }
