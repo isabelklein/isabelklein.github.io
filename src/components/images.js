c1a7f951e3bf29fc43149d76e376a426c98c78fb
Isabel Klein <52258290+isabelklein@users.noreply.github.com>
2020-03-31 00:07:27 -0700

    Fix units, edit projects

---
+++ src/components/images.js
@@ -0,0 +1,9 @@
+const images = [
+    { id: 1, src: '../images/project3/1.png', alt: 'test' },
+    { id: 2, src: '/Users/isabelklein/Documents/GitHub/Portfolio/src/images/project3/1.png', alt: '' },
+    { id: 3, src: '/Users/isabelklein/Documents/GitHub/Portfolio/src/images/project3/1.png', alt: '' },
+    { id: 4, src: '/Users/isabelklein/Documents/GitHub/Portfolio/src/images/project3/1.png', alt: ''  },
+    { id: 5, src: '/Users/isabelklein/Documents/GitHub/Portfolio/src/images/project3/1.png', alt: ''},
+  ];
+  
+  export default images;
--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Project1 from '../projects/project1.js';
 import Project2 from '../projects/project2.js';
 import Project3 from '../projects/project3.js';
-import Project4 from '../projects/project4.js';
+// import Project4 from '../projects/project4.js';
 
 class Project extends Component {
     state = {  }
@@ -16,7 +16,7 @@ class Project extends Component {
             <Route exact path="/project1" component={Project1} />
             <Route path="/project2" component={Project2} />
             <Route path="/project3" component={Project3} />
-            <Route path="/project4" component={Project4} />
+            {/* <Route path="/project4" component={Project4} /> */}
             
             <Fade bottom>
             <div className='thumbnail'>
@@ -30,21 +30,5 @@ class Project extends Component {
         );
     }
 }
-
-// class Project extends Component {
-//     state = {  }
-
-//     render() { 
-//         return (          
-//             <Fade bottom>
-//             <div className='project'>
-//             <a href='/Users/isabelklein/Documents/Github/Portfolio/src/projects/project1.html'>
-//                 <img src={this.props.imageSrc} alt={this.props.title}></img>
-//             </a>
-//             </div> 
-//             </Fade>
-//         );
-//     }
-// }
  
 export default Project;
